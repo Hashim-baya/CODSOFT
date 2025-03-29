97e0b5ff17f8466c1e974387509e21f72cca159c
hashim-baya <bayahashim40@gmail.com>
2025-03-29 03:33:44 +0300

    Updated experience section

--- PERSONAL-PORTFOLIO-WEBSITE/src/components/common/Experience.jsx
+++ PERSONAL-PORTFOLIO-WEBSITE/src/components/common/Experience.jsx
@@ -30,22 +30,41 @@ function Experience() {
     <section>
         <h3 className='text-transparent bg-clip-text bg-gradient-to-r from-[#5BADFF] to-[#1373D1] font-bold text-xl md:text-2xl text-center mb-10'>Experience With</h3>
 
-        <div className='flex justify-center items-center mx-6 md:mx-15 lg:mx-30 xl:mx-60 2xl:mx-80 '>
+        <div className='flex justify-center items-center mx-6 md:mx-15 lg:mx-30 xl:mx-60 2xl:mx-80  '>
             <Carousel 
                 responsive={responsive} 
                 infinite={true} 
                 autoPlay={true} 
                 autoPlaySpeed={2000} 
-                swipeable={false}
-                draggable={false}
+                className='w-full '
+                containerClass="carousel-container"
                 removeArrowOnDeviceType={["tablet", "mobile", "superLargeDesktop", "desktop"]}
+                itemClass="carousel-item-padding-20-px"
             >
+              <div>
                 <FontAwesomeIcon icon={faReact} size="2xl" className='text-[#61DAFB]'  />
+              </div>
+
+              <div>
                 <SiTailwindcss  className='text-[#06B6D4] text-4xl'  />
+              </div>
+
+              <div>
                 <FontAwesomeIcon icon={faJs} size="2xl" className='text-[#F7DF1E]'  />
+              </div>
+               
+              <div>
                 <FontAwesomeIcon icon={faHtml5} size="2xl" className='text-[#E34F26]'  />
+              </div>
+                
+              <div>
                 <FontAwesomeIcon icon={faCss3} size="2xl" className='text-[#1572B6]'  />
+              </div>
+                
+              <div>
                 <FontAwesomeIcon icon={faGitAlt} size="2xl" className='text-[#F1502F]'  />
+              </div>
+                
             </Carousel>
         </div>
     </section>
